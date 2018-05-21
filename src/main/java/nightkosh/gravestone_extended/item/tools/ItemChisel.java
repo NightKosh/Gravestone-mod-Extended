37fddada4c2610486e08c223446ea56becbc81a8
NightKosh <nightkosh@gmail.com>
2018-05-22 01:23:25 +0300

    Fixed gui activation, #3

--- src/main/java/nightkosh/gravestone_extended/item/tools/ItemChisel.java
+++ src/main/java/nightkosh/gravestone_extended/item/tools/ItemChisel.java
@@ -8,6 +8,7 @@ import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import nightkosh.gravestone.tileentity.TileEntityGrave;
 import nightkosh.gravestone_extended.ModGravestoneExtended;
@@ -35,34 +36,35 @@ public class ItemChisel extends ItemTool {
         this.setRegistryName(ModInfo.ID, "gschisel");
     }
 
-    /**
-     * Called whenever this item is equipped and the right mouse button is
-     * pressed. Args: itemStack, world, entityPlayer
-     */
     @Override
     public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
         ItemStack itemStack = player.getHeldItem(hand);
         player.setActiveHand(EnumHand.MAIN_HAND);
-        return new ActionResult(EnumActionResult.PASS, itemStack);
+
+        RayTraceResult raytraceresult = this.rayTrace(world, player, false);
+
+        if (raytraceresult != null && raytraceresult.typeOfHit == RayTraceResult.Type.BLOCK &&
+                (world.getBlockState(raytraceresult.getBlockPos()).getBlock() == GSBlock.GRAVE_STONE ||
+                world.getBlockState(raytraceresult.getBlockPos()).getBlock() == GSBlock.MEMORIAL)) {
+            return new ActionResult<>(EnumActionResult.PASS, itemStack);
+        } else {
+            player.openGui(ModGravestoneExtended.instance, GuiHandler.CHISEL_CRAFTING_GUI_ID, world,
+                    player.getPosition().getX(), player.getPosition().getY(), player.getPosition().getZ());
+            return new ActionResult<>(EnumActionResult.SUCCESS, itemStack);
+        }
     }
 
-    /**
-     * Callback for item usage. If the item does something special on right
-     * clicking, he will have one of those. Return True if something happen and
-     * false if it don't. This is for ITEMS, not BLOCKS
-     */
     @Override
-    public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
+    public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing
+            side, float hitX, float hitY, float hitZ) {
         ItemStack stack = player.getHeldItem(hand);
         if (world.getBlockState(pos).getBlock().equals(GSBlock.GRAVE_STONE)) {
             return setGraveText(stack, player, world, pos, false);
         } else if (world.getBlockState(pos).getBlock().equals(GSBlock.MEMORIAL)) {
             return setGraveText(stack, player, world, pos, true);
-        } else {
-            player.openGui(ModGravestoneExtended.instance, GuiHandler.CHISEL_CRAFTING_GUI_ID, world, pos.getX(), pos.getY(), pos.getZ());
         }
 
-        return EnumActionResult.SUCCESS;
+        return EnumActionResult.FAIL;
     }
 
     private EnumActionResult setGraveText(ItemStack stack, EntityPlayer player, World world, BlockPos pos, boolean isMemorial) {
