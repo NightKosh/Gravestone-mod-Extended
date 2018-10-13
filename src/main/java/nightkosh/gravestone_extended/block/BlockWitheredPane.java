a587e57785afb280ede11923a9fb25f4eb28bfc3
NightKosh <nightkosh@gmail.com>
2018-10-13 23:17:10 +0300

    Make withered glass undestructable by mobs, #114

--- src/main/java/nightkosh/gravestone_extended/block/BlockWitheredPane.java
+++ src/main/java/nightkosh/gravestone_extended/block/BlockWitheredPane.java
@@ -2,7 +2,11 @@ package nightkosh.gravestone_extended.block;
 
 import net.minecraft.block.BlockPane;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import nightkosh.gravestone_extended.core.GSTabs;
@@ -29,5 +33,10 @@ public class BlockWitheredPane extends BlockPane {
     @SideOnly(Side.CLIENT)
     public BlockRenderLayer getBlockLayer() {
         return BlockRenderLayer.TRANSLUCENT;
+    }
+
+    @Override
+    public boolean canEntityDestroy(IBlockState state, IBlockAccess world, BlockPos pos, Entity entity) {
+        return false;
     }
 }
