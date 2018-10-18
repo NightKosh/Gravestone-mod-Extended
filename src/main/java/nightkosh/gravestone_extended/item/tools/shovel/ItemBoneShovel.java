c015d7814ef17da7112643ccdfb15513656fcc8c
NightKosh <nightkosh@gmail.com>
2018-10-19 01:42:44 +0300

    Fix #167

--- src/main/java/nightkosh/gravestone_extended/item/tools/shovel/ItemBoneShovel.java
+++ src/main/java/nightkosh/gravestone_extended/item/tools/shovel/ItemBoneShovel.java
@@ -1,7 +1,7 @@
 package nightkosh.gravestone_extended.item.tools.shovel;
 
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemPickaxe;
+import net.minecraft.item.ItemSpade;
 import net.minecraft.item.ItemStack;
 import nightkosh.gravestone_extended.core.GSBlock;
 import nightkosh.gravestone_extended.core.GSTabs;
@@ -13,7 +13,7 @@ import nightkosh.gravestone_extended.core.ModInfo;
  * @author NightKosh
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
  */
-public class ItemBoneShovel extends ItemPickaxe implements IBoneShovel {
+public class ItemBoneShovel extends ItemSpade implements IBoneShovel {
 
     public ItemBoneShovel() {
         this(ToolMaterial.STONE, "bone_shovel");
