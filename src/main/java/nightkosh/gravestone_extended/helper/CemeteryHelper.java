8b12f74a0e5577532377378a62542425b33473be
NightKosh <nightkosh@gmail.com>
2018-11-02 00:28:05 +0300

    Grave position, #175

--- src/main/java/nightkosh/gravestone_extended/helper/CemeteryHelper.java
+++ src/main/java/nightkosh/gravestone_extended/helper/CemeteryHelper.java
@@ -12,9 +12,13 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import nightkosh.gravestone.api.GraveStoneAPI;
 import nightkosh.gravestone.api.grave_position.IGravePositionHandler;
+import nightkosh.gravestone.helper.GraveGenerationHelper;
 import nightkosh.gravestone_extended.capability.cemetery.CemeteryProvider;
 import nightkosh.gravestone_extended.capability.cemetery.ICemetery;
 import nightkosh.gravestone_extended.core.GSBlock;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * GraveStone mod
@@ -62,10 +66,7 @@ public class CemeteryHelper {
 
             @Override
             public BlockPos gravePosition(World world, Entity entity, BlockPos pos, DamageSource damageSource) {
-                ICemetery cemetery = entity.getCapability(CemeteryProvider.CEMETERY_CAP, null);
-                BlockPos gravePos = cemetery.getPlayerPosition();
-                //TODO
-                return pos.west(2);
+                return getGravePos(world, entity.getCapability(CemeteryProvider.CEMETERY_CAP, null).getPlayerPosition(), false, true);
             }
 
             @Override
@@ -100,10 +101,7 @@ public class CemeteryHelper {
 
             @Override
             public BlockPos gravePosition(World world, Entity entity, BlockPos pos, DamageSource damageSource) {
-                ICemetery cemetery = entity.getCapability(CemeteryProvider.CEMETERY_CAP, null);
-                BlockPos gravePos = cemetery.getPetPosition();
-                //TODO
-                return pos.west(2);
+                return getGravePos(world, entity.getCapability(CemeteryProvider.CEMETERY_CAP, null).getPetPosition(), false, false);
             }
 
             @Override
@@ -132,10 +130,7 @@ public class CemeteryHelper {
 
             @Override
             public BlockPos gravePosition(World world, Entity entity, BlockPos pos, DamageSource damageSource) {
-                ICemetery cemetery = world.getCapability(CemeteryProvider.CEMETERY_CAP, null);
-                BlockPos gravePos = cemetery.getPlayerPosition();
-                //TODO
-                return pos.west(2);
+                return getGravePos(world, world.getCapability(CemeteryProvider.CEMETERY_CAP, null).getPlayerPosition(), true, true);
             }
 
             @Override
@@ -164,10 +159,7 @@ public class CemeteryHelper {
 
             @Override
             public BlockPos gravePosition(World world, Entity entity, BlockPos pos, DamageSource damageSource) {
-                ICemetery cemetery = world.getCapability(CemeteryProvider.CEMETERY_CAP, null);
-                BlockPos gravePos = cemetery.getPetPosition();
-                //TODO
-                return pos.west(2);
+                return getGravePos(world, world.getCapability(CemeteryProvider.CEMETERY_CAP, null).getPetPosition(), true, false);
             }
 
             @Override
@@ -175,5 +167,55 @@ public class CemeteryHelper {
                 return world.getCapability(CemeteryProvider.CEMETERY_CAP, null).getPetFacing();
             }
         });
+    }
+
+    private static final int CEMETERY_SIZE_PLAYER = 4;
+    private static final int CEMETERY_SIZE_PET = 3;
+    private static final int CEMETERY_SIZE_PLAYER_SERVER = 10;
+    private static final int CEMETERY_SIZE_PET_SERVER = 5;
+
+    private static BlockPos getGravePos(World world, BlockPos pos, boolean isServer, boolean playerCemetery) {
+        List<BlockPos> list = new ArrayList<>();
+
+        int xPos;
+        int zPos;
+
+        int size;
+        if (isServer) {
+            size = playerCemetery ? CEMETERY_SIZE_PLAYER_SERVER : CEMETERY_SIZE_PET_SERVER;//TODO server
+        } else {
+            size = playerCemetery ? CEMETERY_SIZE_PLAYER : CEMETERY_SIZE_PET;
+        }
+
+        for (int x = 1; x <= size; x++) {
+            for (int z = 1; z <= size; z++) {
+                if (x == 1 && z == 1) {
+                    continue;
+                }
+                xPos = x * 2 - 1;
+                zPos = z * 2 - 1;
+
+                addPosition(list, world, pos, xPos, zPos);
+                addPosition(list, world, pos, -xPos, zPos);
+                addPosition(list, world, pos, xPos, -zPos);
+                addPosition(list, world, pos, -xPos, -zPos);
+            }
+        }
+
+        if (list.isEmpty()) {
+            return null;
+        } else {
+            return list.get(world.rand.nextInt(list.size()));
+        }
+    }
+
+    private static void addPosition(List<BlockPos> list, World world, BlockPos cemeteryPos, int x, int z) {
+        x = cemeteryPos.getX() + x;
+        z = cemeteryPos.getZ() + z;
+
+        int y = GraveGenerationHelper.getGround(world, x, cemeteryPos.getY(), z);
+        if (GraveGenerationHelper.canGenerateGraveAtCoordinates(world, new BlockPos(x, y, z))) {
+            list.add(new BlockPos(x, y, z));
+        }
     }
 }
