07587a6b8445715b0e69c1f0752a29b932202c43
NightKosh <nightkosh@gmail.com>
2018-11-13 03:17:14 +0300

    fix, #79

---
+++ src/main/java/nightkosh/gravestone_extended/item/armor/bone/chestplate/IBoneChestplate.java
@@ -0,0 +1,12 @@
+package nightkosh.gravestone_extended.item.armor.bone.chestplate;
+
+import nightkosh.gravestone_extended.item.armor.bone.IBoneArmor;
+
+/**
+ * GraveStone mod
+ *
+ * @author NightKosh
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ */
+public interface IBoneChestplate extends IBoneArmor {
+}
