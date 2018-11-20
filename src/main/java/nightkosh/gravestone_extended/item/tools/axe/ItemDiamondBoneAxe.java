2788796210301daaf978bf62399d0f0e1ce4bcb6
NightKosh <nightkosh@gmail.com>
2018-11-21 02:06:05 +0300

    axe material, #185

--- src/main/java/nightkosh/gravestone_extended/item/tools/axe/ItemDiamondBoneAxe.java
+++ src/main/java/nightkosh/gravestone_extended/item/tools/axe/ItemDiamondBoneAxe.java
@@ -1,6 +1,11 @@
 package nightkosh.gravestone_extended.item.tools.axe;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemAxe;
+import net.minecraft.item.ItemStack;
+import nightkosh.gravestone_extended.core.GSBlock;
+import nightkosh.gravestone_extended.core.GSMaterials;
+import nightkosh.gravestone_extended.core.GSTabs;
 import nightkosh.gravestone_extended.core.ModInfo;
 
 /**
@@ -9,12 +14,17 @@ import nightkosh.gravestone_extended.core.ModInfo;
  * @author NightKosh
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
  */
-public class ItemDiamondBoneAxe extends ItemBoneAxe {
+public class ItemDiamondBoneAxe extends ItemAxe implements IBoneAxe {
 
     public ItemDiamondBoneAxe() {
-//        super(GSMaterials.BONE_TOOL); //TODO !!!!!!!!!!
-        super(Item.ToolMaterial.DIAMOND);
+        super(GSMaterials.BONE_TOOL, 9, -2.9F);
         this.setUnlocalizedName("gravestone.bone_axe_diamond");
         this.setRegistryName(ModInfo.ID, "gs_bone_axe_diamond");
+        this.setCreativeTab(GSTabs.otherItemsTab);
+    }
+
+    @Override
+    public boolean getIsRepairable(ItemStack toRepair, ItemStack repair) {
+        return super.getIsRepairable(toRepair, repair) || repair.getItem() == Item.getItemFromBlock(GSBlock.BONE_BLOCK);
     }
 }
